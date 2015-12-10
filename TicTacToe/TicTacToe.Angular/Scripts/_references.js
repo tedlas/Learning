6f5bacdc5bf852dd173acc43e13a153c7acae139
tedlas <rstedla@gmail.com>
2015-12-10 12:51:25 -0800

    Adding the TicTacToe Angular project

---
+++ TicTacToe/TicTacToe.Angular/Scripts/_references.js
@@ -0,0 +1,6 @@
+﻿/// <autosync enabled="true" />
+/// <reference path="jquery-2.1.4.js" />
+/// <reference path="angular-mocks.js" />
+/// <reference path="angular.js" />
+/// <reference path="../app/app.js" />
+/// <reference path="bootstrap.min.js" />
